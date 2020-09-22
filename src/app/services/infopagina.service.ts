439714709314552f85f8f207a6d31baf2171a608
Andres <jhohan.andres83@gmail.com>
2020-09-22 13:54:17 -0500

    peticiones http a Firebase

--- src/app/services/infopagina.service.ts
+++ src/app/services/infopagina.service.ts
@@ -1,23 +1,37 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { infoequipo } from '../interfaces/info-equipoInterfaces';
 import { infoPagina } from '../interfaces/info-pagina.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfopaginaService {
-  info : infoPagina ={};
+  info: infoPagina = {};
   cargada = false;
+  equipo: infoequipo = {};
+  constructor(private http: HttpClient) {
+    //console.log("Servicio de la pagina listo");
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
-  constructor( private http: HttpClient) {
-   //console.log("Servicio de la pagina listo");
-    //Leer el archivo Json
-
+  private cargarInfo() {
     this.http.get('assets/data/data-pagina.json')
-    .subscribe( (res: infoPagina) => {
-      this.cargada=true;
+    .subscribe((res: infoPagina) => {
+      this.cargada = true;
       this.info = res;
       console.log(res);
     })
   }
+
+  private cargarEquipo() {
+    this.http.get('https://angular-html-daa33.firebaseio.com/equipo.json')
+    .subscribe((res: infoequipo) => {
+      this.cargada = true;
+      this.equipo = res;
+      console.log(res);
+    })
+  }
+  
 }
